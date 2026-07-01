2b7e689e3ce23e020bc244f2a49aa86b17e32968
mfelixc <mfelixc@unjbg.edu.pe>
2026-06-30 22:17:15 -0500

    Se implementa el registro de datos de las personas.

--- propuesto3.cpp
+++ propuesto3.cpp
@@ -17,3 +17,20 @@ int main() {
     cin >> n;
 
     Persona personas[n];
+    for (int i = 0; i < n; i++) {
+        cin.ignore();
+
+        cout << "\nPersona " << i + 1 << endl;
+
+        cout << "Nombre: ";
+        getline(cin, personas[i].nombre);
+
+        cout << "Dia de nacimiento: ";
+        cin >> personas[i].dia;
+
+        cout << "Mes de nacimiento: ";
+        cin >> personas[i].mes;
+
+        cout << "Anio de nacimiento: ";
+        cin >> personas[i].anio;
+    }
